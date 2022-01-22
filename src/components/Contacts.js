aafb11845d560ae3d955d7e90a5be4bece5e42db
kencal20 <kenadjei20@gmail.com>
2022-01-22 19:54:47 +0000

    contacts js

---
+++ src/components/Contacts.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Contacts = () => {
+  return (
+    <div>
+          <h1>Contacts</h1>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et nunc sed
+      nunc sollicitudin tempor a id diam. Etiam porttitor nulla augue, a
+      venenatis tellus pretium nec. Phasellus vel ipsum quis purus eleifend
+      consectetur quis sed augue. Quisque arcu ante, lobortis a dui in, suscipit
+      maximus tellus. Proin lobortis odio erat, ac tristique odio finibus sit
+      amet. Duis tempus mauris nec nulla iaculis, eu gravida massa dignissim.
+      Aenean sit amet viverra sapien. Maecenas gravida elit at lacus tempus
+      consequat vel in quam. Nullam nec augue risus
+    </div>
+  );
+};
+
+export default Contacts;
