36ed796f9e639d482a4da1f456c0ca225769ca19
kencal20 <kenadjei20@gmail.com>
2022-01-23 12:08:54 +0000

    Login update

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,25 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
 import "../Main_styles.css";
 import { Link } from "react-router-dom";
-const Login = () => {
-  return (
-    <div className="App">
-      <h1>Login</h1>
-      <p>Hello member</p>
-      <form>
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Username: "",
+      Password: "",
+    };
+  }
+  handlechange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    this.setstate = {
+      Username: "",
+      Password: "",
+    };
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Login</h1>
+        <p>Hello member</p>
         <div className="User-name">
-          <input type="text" placeholder="User Name " />
+          <input
+            type="text"
+            placeholder="User Name "
+            name="Firstname"
+            value={this.state.Username}
+            onChange={this.handlechange}
+          />
         </div>
         <div className="Password">
-          <input type="password" placeholder="Password" />
+          <input
+            type="password"
+            placeholder="Password"
+            name="Password"
+            value={this.state.Username}
+            onChange={this.handlechange}
+          />
+          <div>
+            <input type ="reset"/>
+            <input type="submit"/>
+          </div>
+          <p>New Here?</p>
+          <Link to="/signup">
+            <button type="button">Signup</button>
+          </Link>
         </div>
-      </form>
-      <p>New Here?</p>
-      <Link to="/signup">
-        <p>Signup</p>
-      </Link>
-    </div>
-  );
-};
-
+      </div>
+    );
+  }
+}
 export default Login;
