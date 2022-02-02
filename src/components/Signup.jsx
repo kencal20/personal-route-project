5f77941fce910e58452144c54b91e7712c309260
kencal20 <kenadjei20@gmail.com>
2022-02-02 09:00:58 +0000

    signup update

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,21 +2,21 @@ import React, { useState } from "react";
 import { useUserAuth } from "../Context/UserAuthContext";
 import { Button } from "react-bootstrap";
 
-
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setpassword] = useState("");
   const [error, seterror] = useState("");
-  const { signup } = useUserAuth();
-
+  const  signup  = useUserAuth();
   return (
     <>
       <form>
-        <h1>Signup from</h1>
+        <h1>Signup form</h1>
         <label>Email</label>
-        <input type="email" value={email} />
+        <input type="email" />
+        <br />
         <label>Password</label>
-        <input type="password" value={password} />
+        <input type="password" />
+        <br />
         <Button variant="primary">Submit</Button>
       </form>
     </>
